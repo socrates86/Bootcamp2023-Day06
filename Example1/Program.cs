83563048688eca386ffab078602695fb0d7ec347
socrates86 <morrisonikechukwu86@gmail.com>
2024-02-12 22:28:04 +0100

    First commit

---
+++ Example1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Example1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("What is your fvourite continent?");
+        string? choice = Console.ReadLine();
+        choice = choice?.ToLower();
+
+        const string Africa = "africa";
+        const string Asia = "asia";
+        const string SouthAmerica = "southamerica";
+        const string Europe = "europe";
+        const string NorthAmerica = "northamerica";
+
+        switch(choice)
+        {
+            case Africa :
+            Console.WriteLine("Halo Africa, you're in deed blessed!");
+            break;
+
+            case Asia :
+            Console.WriteLine("Halo Asia, how do you do today?");
+            break;
+
+            case SouthAmerica :
+            Console.WriteLine("Halo SouthAmerica, what's up with you?");
+            break;
+
+            case  Europe :
+            Console.WriteLine("Halo Europe, I love you!");
+            break;
+
+            case NorthAmerica :
+            Console.WriteLine("Halo NorthAmerica, you have evrything it takes.");
+            break;
+
+            default :
+            Console.WriteLine("Please choose a known option");
+            break;
+        }
+    }
+}
---
+++ Example2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Example2;
+
+class Program
+{
+    static Void Main(string[] args)
+    {
+        Console.WriteLine("Which from the options below is your favourite continent?");
+        console.WriteLine("1. Africa \n2. North America \n3. Asia \n4. Europe");
+        int? choice = Convert.ToLower(Console.ReadLine());
+    }   
+
+    switch (choice)
+    {
+        case 1 :
+        Console.WriteLine("Halo Africa, you're great!");
+        break;
+
+        case 2 :
+        Console.WriteLine("Halo North America, what's going on?");
+        break;
+
+        case 3 :
+        Console.WriteLine("Halo Asia, make peace with your opposition now");
+        break;
+
+        case 4 :
+        Console.WriteLine("Halo Europe, what's up with you?");
+        break; 
+
+        default:
+        Console.WriteLine("Halo, please provide a known option.");
+        break;
+    }
+}
